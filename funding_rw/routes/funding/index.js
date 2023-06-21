20eb00913bafd4bab1b14d23ca20a523854b7bd7
nognuj <nognuj@gmail.com>
2023-06-21 12:38:56 +0900

    [Add] funding_rw POST

--- funding_rw/routes/funding/index.js
+++ funding_rw/routes/funding/index.js
@@ -4,4 +4,24 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: '/funding read write version3' }
   })
+
+  fastify.post('/', async function (request, reply) {
+
+
+    const requestBody = request.body;
+    // console.log(requestBody);
+
+    // JSON.parse(): JSON 문자열을 JavaScript 객체로 변환
+    let parseBody = JSON.parse(requestBody);
+    // console.log(parseBody);
+    
+    
+    let ret = parseBody.body +' success';
+  
+    // Set the response status code
+    reply.code(201);
+    // Send the request body as the response
+    return { ret: ret};
+  })
+
 }
