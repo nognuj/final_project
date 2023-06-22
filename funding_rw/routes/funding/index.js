0253f3986ef02be2fae3285a7e0740611e198d7a
nognuj <nognuj@gmail.com>
2023-06-22 20:43:11 +0900

    [Test] number

--- funding_rw/routes/funding/index.js
+++ funding_rw/routes/funding/index.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/funding read write ci/cd  push 2번 하는게 맞는 걸까?! wlsWKfh %$%#$%$#' }
+    return { root: '/funding read write ci/cd  push 2번 하는게 맞는 걸까?! wlsWKfh %$%#$%$# 0987654321' }
   })
 
   fastify.post('/createDB', async function (request, reply) {
