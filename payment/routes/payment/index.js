23a5525863c2c423436e7b59641fe1f21f7c0ccd
nognuj <nognuj@gmail.com>
2023-06-22 20:55:43 +0900

    [Test payment]

--- payment/routes/payment/index.js
+++ payment/routes/payment/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/payment ci / cd branch test 이것이 진정한 정답이란 말인가./... 안녕할 수 없어요 죄송합니다' }
+    return { root: '/payment ci / cd branch test 이것이 진다' }
   })
 }
