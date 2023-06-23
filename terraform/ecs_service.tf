3172d25539c46939515d00ffe33a3c1fa9e76e51
nognuj <nognuj@gmail.com>
2023-06-24 02:57:16 +0900

    [Add] ECS

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -1,20 +1,54 @@
 # # aws_ecs_cluster_serivce
-# resource "aws_ecs_service" "ecs_serivce" {
-#   name            = "ecs_serivce"
-#   cluster         = aws_ecs_cluster.crowd_cluster.id
-#   task_definition = aws_ecs_task_definition.ecs_td_crowd.arn
-#   desired_count   = 3
-#   iam_role        = aws_iam_role.test_role.arn
-#   depends_on      = [aws_iam_role_policy.test_policy]
+resource "aws_ecs_service" "terraform_ecs_serivce" {
+  name            = "terraform_ecs_serivce"
+  cluster         = aws_ecs_cluster.crowd_cluster.id
+  task_definition = aws_ecs_task_definition.terraform_td_funding.arn
+  desired_count   = 1
+  launch_type = "FARGATE"
 
-#   ordered_placement_strategy {
-#     type  = "binpack"
-#     field = "cpu"
-#   }
+  network_configuration {
+    security_groups = [aws_security_group.terraform_sg.id]
+    subnets = [aws_subnet.pubSub1.id, aws_subnet.pubSub2.id]
+    assign_public_ip = true
+  }
+  # iam_role        = aws_iam_role.test_role.arn
+  # depends_on      = [aws_iam_role_policy.test_policy]
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.alb_tg.arn
-#     container_name   = "mongo"
-#     container_port   = 8080
-#   }
-# }
+  # ordered_placement_strategy {
+  #   type  = "binpack"
+  #   field = "cpu"
+  # }
+
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.alb_tg.arn
+  #   container_name   = "mongo"
+  #   container_port   = 8080
+  # }
+}
+
+resource "aws_security_group" "terraform_sg" {
+  name        = "terraform_vpc"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.lastvpc.id
+
+  ingress {
+    description      = "TCP"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
