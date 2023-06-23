16eb6a912ba585131cfd1da7f5d4608d80ae90bf
nognuj <nognuj@gmail.com>
2023-06-23 16:59:35 +0900

    [Fix] dir structure

--- payment/routes/payment/index.js
+++ payment/routes/payment/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/payment ci / cd branch test 이것이 wlfRK?' }
+    return { root: '/payment ci / cd branch test 이것이 wlfRK? 제주도는 어때?' }
   })
 }
