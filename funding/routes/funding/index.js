c2e72783370ae348a21b3732174fcdca40f71318
nognuj <nognuj@gmail.com>
2023-06-27 00:56:52 +0900

    [Test] funding

--- funding/routes/funding/index.js
+++ funding/routes/funding/index.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/funding read only 이게 마지막 test' }
+    return { root: '/funding read only 이게 마지막 test REAL' }
   })
 
   // fastify.get('/get', async function (request, reply) {
