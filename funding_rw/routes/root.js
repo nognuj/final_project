6e7d999089e70d6b94a503653b145c7dedf0923e
nognuj <nognuj@gmail.com>
2023-06-27 01:48:16 +0900

    [Upstream]

--- funding_rw/routes/root.js
+++ funding_rw/routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: 'root funding rw 입니다 LAST_TEST' }
+    return { root: 'root funding rw 입니다 아니 없어' }
   })
 }
