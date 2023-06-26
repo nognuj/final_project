99cc29a67a08ac96463392369f1c891a21de8e51
nognuj <nognuj@gmail.com>
2023-06-27 01:42:21 +0900

    [funding RW Test]

--- funding_rw/routes/funding/index.js
+++ funding_rw/routes/funding/index.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/funding read write ecs test입니다 0628' }
+    return { root: '/funding read write ecs test입니다 last_test' }
   })
 
   //gahyun-funding added
