b861c4d3d230dafbd461b4baea28725fb5833eab
nognuj <nognuj@gmail.com>
2023-06-27 01:50:49 +0900

    [Upstream]

--- funding/routes/root.js
+++ funding/routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: 'root funding read 일까요 FAKE 1' }
+    return { root: 'root funding read 일까요 배고픈 사자' }
   })
 }
