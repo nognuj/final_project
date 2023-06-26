8b22a94ffa07b8a2c1eff67b1362931c1095a350
nognuj <nognuj@gmail.com>
2023-06-27 03:24:25 +0900

    [Test] payment ecs

--- payment/routes/payment/index.js
+++ payment/routes/payment/index.js
@@ -2,6 +2,10 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/payment 가보자잇' }
+    return { root: '/payment 어디로 떠나볼까' }
+  })
+
+  fastify.get('/abc', async function (request, reply) {
+    return { root: '/payment 우주로가쨔!' }
   })
 }
