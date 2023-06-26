60c763b01c17266535d3b89ab3cafc3c36d7296e
nognuj <nognuj@gmail.com>
2023-06-26 23:52:14 +0900

    [Fix] ecr tr

--- funding_rw/routes/funding/index.js
+++ funding_rw/routes/funding/index.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/funding read write ecs test' }
+    return { root: '/funding read write ecs test2' }
   })
 
   //gahyun-funding added
