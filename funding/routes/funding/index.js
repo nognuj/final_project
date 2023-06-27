fe48774b38aa55b4eb6ab52517a5c06610c1b7d1
GahyunSong <gahyun8876@gmail.com>
2023-06-27 17:08:34 +0900

    [create] create funding folder

--- funding/routes/funding/index.js
+++ funding/routes/funding/index.js
@@ -2,23 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: '/funding read only 사나운 고양이' }
+    return { root: '/funding read only 뭐를 바꿔요?ㅎ' }
   })
 
-  // fastify.get('/get', async function (request, reply) {
-
-  //   // db connection
-  //   const connection = await fastify.mysql.getConnection()
-
-  //   // 일단 추출
-  //   const [id, name, email] = await connection.query(
-  //     `SELECT * FROM test.test01`
-  //   )
-  //   connection.release()
-  
-  //   // Set the response status code
-  //   reply.code(200);
-  //   // Send the request body as the response
-  //   return { port: 'funding', ret: id};
-  // })
 }
