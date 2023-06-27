c87e028027157843feb5e28299e57ca2311bad3c
GahyunSong <gahyun8876@gmail.com>
2023-06-27 16:50:18 +0900

    [fix] modify all codes in this folder

--- funding_rw/plugins/sensible.js
+++ funding_rw/plugins/sensible.js
@@ -8,7 +8,7 @@ const fp = require('fastify-plugin')
  * @see https://github.com/fastify/fastify-sensible
  */
 const {
-  DB_USERNAME,
+  DB_USERNAME = "",
   DB_PASSWORD,
   DB_HOST,
   DB_TALBE
@@ -21,6 +21,7 @@ module.exports = fp(async function (fastify, opts) {
 
   fastify.register(require('@fastify/mysql'), {
     promise: true,
-    connectionString: `mysql://admin:12345678@database-2.cnd9cstsmizu.ap-northeast-2.rds.amazonaws.com/test`
+    connectionString: `mysql://admin:12345678@terrdb.cnd9cstsmizu.ap-northeast-2.rds.amazonaws.com/test`
+    //connectionString: `mysql://admin:12345678@database-2.cnd9cstsmizu.ap-northeast-2.rds.amazonaws.com/test`
   })
 })
