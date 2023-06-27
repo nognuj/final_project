3ce3587853d41610929a56cb9e8dd498eb1d9758
GahyunSong <gahyun8876@gmail.com>
2023-06-27 13:07:08 +0900

    [fix] modifiy code

--- funding_rw/routes/api/funding/index.js
+++ funding_rw/routes/api/funding/index.js
@@ -2,14 +2,13 @@
 
 module.exports = async function (fastify, opts) {
 
-  // GET -> /api/fundings
+  // GET -> /api/funding
   fastify.get('/', async function (request, reply) {
     
     try{
        // db connection
       const connection = await fastify.mysql.getConnection();
 
-      // 일단 추출
       const funding_list = await connection.query(
         `SELECT * FROM test.Funding`
       );
@@ -58,8 +57,9 @@ module.exports = async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
     
     try{
-       // db connection
+      // db connection
       const connection = await fastify.mysql.getConnection();
+
       const {title, description, fundingAmount, fundingGoal, minAmount, maxAmount} = request.body;
       const startDate = new Date('2023-06-25T09:00:00');
       const endDate = new Date('2023-07-25T15:30:00');
@@ -69,8 +69,7 @@ module.exports = async function (fastify, opts) {
       const createdAt = new Date();
       let status = 1; //funding is an inactive by default
 
-     // Insert Data
-     // fundingId is created automatically according to the database table
+      // Insert Data
       const funding_list = await connection.query(
         `INSERT INTO test.Funding (title, description, duration, startDate, endDate, createdAt, status, fundingAmount,fundingGoal, minAmount, maxAmount)
          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?)`,
@@ -113,6 +112,8 @@ module.exports = async function (fastify, opts) {
   //PUT method for a certain funding proj
   fastify.put('/:fundingId', async function (request, reply) {
     try {
+      // const API_GATEWAY_URL = "https://7gnfo6nmjj.execute-api.ap-northeast-2.amazonaws.com/Prod/sns/publication/pg";
+
       const fundingId = request.params.fundingId;
       const { fundingAmount } = request.body;
   
@@ -120,9 +121,7 @@ module.exports = async function (fastify, opts) {
       
       // update a funding amount
       const updatedQuery = 'UPDATE test.Funding SET fundingAmount = fundingAmount + ? WHERE fundingId = ?';
-      const updatedData = await connection.execute(updatedQuery, [fundingAmount ,fundingId]);
-      console.log("-------------------------------------------")
-      console.log(updatedData)
+      await connection.execute(updatedQuery, [fundingAmount ,fundingId]);
       
       // get the updated funding amount
       const getQuery = 'SELECT fundingAmount FROM test.Funding WHERE fundingId = ?';
@@ -137,7 +136,10 @@ module.exports = async function (fastify, opts) {
       }
 
       connection.release();
-  
+
+      // const resert = await axios.post(API_GATEWAY_URL,{headers,body});
+      // console.log(resert);
+
       if (result.affectedRows === 0) {
         reply.code(404).send({ error: 'FundingId not found' });
       } else {
@@ -148,90 +150,6 @@ module.exports = async function (fastify, opts) {
       reply.code(500).send({ error: 'Internal server error' });
     }
   });
-  
-//--------------------------------------------------------------------------
-  fastify.post('/createDB', async function (request, reply) {
-    
-    // db connection
-    const connection = await fastify.mysql.getConnection()
-
-    // query 작성
-    const [rows, fields] = await connection.query(
-      'CREATE TABLE test.test01(\
-        id INT NOT NULL AUTO_INCREMENT,\
-        name VARCHAR(255) NOT NULL,\
-        email VARCHAR(255) NOT NULL,\
-        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, \
-        updated_at TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, \
-        PRIMARY KEY (id), \
-        UNIQUE KEY email (email)\
-      );'
-    )
-    connection.release()
-    // return rows[0]
-  
-    // Set the response status code
-    reply.code(201);
-    // Send the request body as the response
-    return { ret: 'create table'};
-  })
-
-
-
-  fastify.post('/insert', async function (request, reply) {
-    
-    // db connection
-    const connection = await fastify.mysql.getConnection()
-
-    // 유저 추가
-    const [rows, fields] = await connection.query(
-      `INSERT INTO test.test01(name, email) VALUES('Cheolwon', 'abcd@email.com')`
-    )
-    connection.release()
-    // return rows[0]
-
-
-    console.log('fin');
-  
-    // Set the response status code
-    reply.code(201);
-    // Send the request body as the response
-    return { ret: 'insert db'};
-  })
-
-  fastify.get('/get', async function (request, reply) {
-
-    // db connection
-    const connection = await fastify.mysql.getConnection()
-
-    // 일단 추출
-    const [id, name, email] = await connection.query(
-      `SELECT * FROM test.test01`
-    )
-    connection.release()
-  
-    // Set the response status code
-    reply.code(200);
-    // Send the request body as the response
-    return { port: 'funding_rw', ret: id};
-  })
-
-  fastify.post('/user', async function (request, reply) {
-
-    // db connection
-    const connection = await fastify.mysql.getConnection()
-
-    // table을 지워버림
-    await connection.query(
-      `DROP TABLE test.test01`
-    )
-    connection.release()
-  
-    // Set the response status code
-    reply.code(201);
-    // Send the request body as the response
-    return { ret : 'delete table' };
-  })
 }
 
 
